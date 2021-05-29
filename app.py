82c6e3ba5635f3b7d7b491dabf048e39bcac8f35
PrathyushaAvula <73877684+PrathyushaAvula@users.noreply.github.com>
2021-05-29 18:09:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World!"
+    return 'Web App with Python Flask!'
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
